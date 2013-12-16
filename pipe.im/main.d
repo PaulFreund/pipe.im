2634147cb5f1f1516a37185346216b32731b91ba
PaulFreund <freund.paul@lvl3.org>
2013-12-17 00:48:24 +0100

    * Updated Todo

--- pipe.im/main.d
+++ pipe.im/main.d
@@ -1,13 +1,21 @@
 import std.stdio;
 
-/* TODO  ===================
+/* TODO  ============================================================================
 
-* Complete derelict glib/purple
- * Make modules out of files
- * Convert function definitions
- * import files in mains
+* Create external package derelict-glib
+    * Create dub package
+    * Publish dub package
 
-==========================*/ 
+* Create external package derelict-purple
+    * Create dub package
+    * Publish dub package
+
+* Create pipe.im pakage
+    * Create dub package
+    * Test on Linux
+    * Publish dub package
+
+===================================================================================*/ 
 
 
 import derelict.glib.glib;
